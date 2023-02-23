9b123e0f40784306bac4e343a2d6eac5153e9170
josephguo <josephguo@josephdembp.dhcp.wustl.edu>
2023-02-23 15:40:32 -0600

    Flag_Studio

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,9 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	StdDraw.setPenColor(Color.BLUE);
+	StdDraw.filledRectangle(.5, 0.5, 0.3, 0.1);
+	StdDraw.setPenColor(Color.ORANGE);
+	StdDraw.filledCircle(.5, 0.5, 0.05);
 	}
 }
